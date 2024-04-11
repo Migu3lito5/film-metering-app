ce26b8b0dd4514cadcba45c913a7b9db6fae0855
Miguel A. Francisco <migufran4594@gmail.com>
2024-04-11 12:36:44 -0700

    Feat: Added some styling to make it look a bit cleaner

--- Navigation.js
+++ Navigation.js
@@ -1,9 +1,12 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer , DefaultTheme} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 import Home from './screen/TabScreens/Home';
 import Rolls from './screen/TabScreens/Rolls';
 import Settings from './screen/TabScreens/Settings';
+
+const navTheme = DefaultTheme;
+navTheme.colors.background = 'white';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +15,12 @@ function TabGroup() {
   <Tab.Navigator
     screenOptions={({route, navigation}) => ({
       headerTitleAlign: 'center',
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: "#7f9179",
+        paddingTop: 10,
+        height: 90,
+      },
       tabBarIcon: ({color, focused, size}) => {
         let iconName;
         let iconColor;
@@ -19,23 +28,23 @@ function TabGroup() {
         switch (route.name) {
           case "Home":
             iconName = "camera";
-            iconColor = focused ? "green" : "black";
-            return <Ionicons name={iconName} size={size} color={color}/>;
+            iconColor = focused ? "#cbfcbb" : "white";
+            return <Ionicons name={iconName} size={size} color={iconColor}/>;
           case "Rolls":
             iconName = 'film';
-            iconColor = focused ? "green" : "black";
-            return <MaterialCommunityIcons name={iconName} size={size} color={color}/>;
+            iconColor = focused ? "#cbfcbb" : "white";
+            return <MaterialCommunityIcons name={iconName} size={size} color={iconColor}/>;
           case "Settings":
             iconName = "settings-sharp";
-            iconColor = focused ? "green" : "black";
-            return <Ionicons name={iconName} size={size} color={color}/>;
+            iconColor = focused ? "#cbfcbb" : "white";
+            return <Ionicons name={iconName} size={size} color={iconColor}/>;
           default:
             return null;
         }
       },
-      tabBarActiveTintColor: "#FF5247",
-    }
-  )}
+      tabBarActiveTintColor: "#cbfcbb",
+      tabBarInactiveTintColor: "white",
+    })}
   >
     <Tab.Screen name="Home" component={Home}/>
     <Tab.Screen name="Rolls" component={Rolls}/>
